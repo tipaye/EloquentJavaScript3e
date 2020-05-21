01cdc728d594ed33bdc0e34f28be4aff7554ccbf
tipaye <40826923+tipaye@users.noreply.github.com>
2020-05-21 12:08:23 +0100

    Finished Bean counting Exercise.

---
+++ Chapter03/BeanCounting/beanCounting.js
@@ -0,0 +1,37 @@
+/*
+// first version, hard-coded for "B"
+function countBs(line)
+{
+    let numBs = 0;
+    for (let ii = 0; ii < line.length; ++ii){
+        if (line[ii] === "B") {
+            numBs +=1;
+        }
+    }
+    
+    return numBs;
+}
+*/
+
+function countChar(line, char)
+{
+    let numChar = 0;
+    for (let ii = 0; ii < line.length; ++ii){
+        if (line[ii] === char) {
+            numChar += 1;
+        }
+    }
+    
+    return numChar;    
+}
+
+///*
+// second version uses general function
+let countBs = line => countChar(line, "B");
+
+// tests
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
